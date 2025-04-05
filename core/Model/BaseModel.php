b0f8641dc2d416c7f205030d5bd7145c0f2b3f6b
DeakinCCCC <163674260+DeakinCCCC@users.noreply.github.com>
2025-04-05 19:56:41 +0800

    更新 BaseModel.php

--- core/Model/BaseModel.php
+++ core/Model/BaseModel.php
@@ -6,9 +6,13 @@ use core\DB\BasePDO;
 class BaseModel extends BasePDO
 {
 
+    protected $table;
+
     public function __construct($table)
     {
-        parent::__construct($table);
+        $config = getMysqlConfig();
+        parent::__construct($config['host'].":".$config['port'],$config['dbname']), $config['username'], $config['password']);
+        $this->$table = $table;
     }
 
 }
