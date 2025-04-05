93d81fec3391ff45762eab98aefadc1495288eaa
DeakinCCCC <163674260+DeakinCCCC@users.noreply.github.com>
2025-04-05 13:20:25 +0800

    更新 autoload.php

--- autoload.php
+++ autoload.php
@@ -1,1 +1,16 @@
 <?php
+
+$classPrefixList = ['app\\', 'core\\'];
+
+foreach ($classPrefixList as $prefix) {
+  spl_autoload_register(function($class) use ($prefix) {
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+      return;
+    }
+    $classFile = __DIR__ . DIRECTORY_SEPARATOR. str_replace('\\', DIRECTORY_SEPARATOR, $class). '.php';
+    if (file_exists($classFile)) {
+      require $classFile;
+    }
+  });
+}
