1cf288d09c12c69c8d48b013a39542e94e20197d
DeakinCCCC <163674260+DeakinCCCC@users.noreply.github.com>
2025-04-05 14:03:23 +0800

    更新

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+//定义应用目录
+const APP_PATH = __DIR__ . DIRECTORY_SEPARATOR;
+
+注册自动加载
+require(APP_PATH . 'autoload.php');
+
+//注册依赖组件自动加载
+//require(APP_PATH . 'vendor/autoload.php');
+
+//注册路由文件
+require(APP_PATH . 'config/routing.php');
+
+//加载公共函数文件
+require(APP_PATH . 'core/function.php');
+
+//实例化框架类并运行
+(new core\Core())->run();
