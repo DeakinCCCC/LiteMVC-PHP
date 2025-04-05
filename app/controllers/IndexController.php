20fa1d9183584392c243bbd92d9f6c1eede7937f
DeakinCCCC <163674260+DeakinCCCC@users.noreply.github.com>
2025-04-05 18:10:19 +0800

    更新

---
+++ app/controllers/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+namespace app\controllers;
+
+use core\Controller\BaseController;
+
+class IndexController extends BaseController
+{
+    public function index(){
+
+        //echo "Hello World!";
+        $this->render();
+        
+    }
+}
