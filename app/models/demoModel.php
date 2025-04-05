dfba032a42855179d3e6fd011b7bfd772b61f253
DeakinCCCC <163674260+DeakinCCCC@users.noreply.github.com>
2025-04-05 23:47:26 +0800

    更新 demoModel.php

--- app/models/demoModel.php
+++ app/models/demoModel.php
@@ -1,1 +1,19 @@
 <?php
+
+namespace app\models;
+
+use core\Model\BaseModel;
+
+class demoModel extends BaseModel
+{
+    public function __construct()
+    {
+        parent::__construct("demo");
+    }
+
+    public function test(){
+
+        return $this->fetchAll("SELECT * FROM `demo`");
+
+    }
+}
